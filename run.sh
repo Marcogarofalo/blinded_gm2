486a4c0ae7b5e2eb1c19d5a5974c00f716a51942
Marcogarofalo <marcogarofalo31@gmail.com>
2024-06-13 14:08:53 +0200

    binning into

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./blinded_gm2 -p ../data/ C80 -bin 1 jack  0.00060
-./blinded_gm2 -p ../data/ B64 -bin 1 jack 0.00072
+./blinded_gm2 -p ../data/ C80 -bin 50 jack  0.00060
+./blinded_gm2 -p ../data/ B64 -bin 50 jack 0.00072
