705a7510120a7e91eb07fa051902c2633f26a261
Marcogarofalo <marcogarofalo31@gmail.com>
2024-06-24 19:30:06 +0200

    D96

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-./blinded_gm2 -p ../data/ C80 -bin 50 jack  0.00060
-./blinded_gm2 -p ../data/ B64 -bin 50 jack 0.00072
+./blinded_gm2 -p ../data/ C80 -bin 50 jack  0.00060 || exit 1
+./blinded_gm2 -p ../data/ B64 -bin 50 jack 0.00072 || exit 1
+./blinded_gm2 -p ../data/ D96 -bin 50 jack 0.00054 || exit 1
+./fit_all_blind jack ../data/jackknife/ ../data/fit_all || exit 1
